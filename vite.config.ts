35bbbf5be070819dd44dc5c75665a24371018f3f
Manav Modi <manavmodi@MAC-06D3EF.station>
2026-01-11 15:17:07 +0000

    chore: setup github actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // IMPORTANT: This must match your repo name exactly
+  // IMPORTANT: The P must be capital to match your repo name exactly
   base: "/Portfolio/", 
   server: {
     host: "::",
