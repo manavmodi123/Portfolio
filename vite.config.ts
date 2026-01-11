94fb5bfa53c3a7a22668f8774dc7e3769ef2b8a4
Manav Modi <manavmodi@MAC-06D3EF.station>
2026-01-11 15:08:05 +0000

    chore: setup github actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // IMPORTANT: This must match your repo name exactly
+  base: "/Portfolio/", 
   server: {
     host: "::",
     port: 8080,
